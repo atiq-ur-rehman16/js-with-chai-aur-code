6d0bb6ee09f5e634031c2c543b957f67cb84dc35
Atiq Ur Rehman <114483766+atiq-ur-rehman16@users.noreply.github.com>
2025-08-21 19:04:46 +0000

    basics 2

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -5,12 +5,12 @@ const score = 400
 const balance = new Number(100)
 // console.log(balance);
 
-// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2)); //decimal point ke baad digits limit karta hai
 
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));//total digits fix karta hai
 
 
 const hundreds = 1000000
@@ -22,8 +22,8 @@ const hundreds = 1000000
 
 // console.log(Math);
 
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
+// console.log(Math.abs(-4));//absolute value (negative ko positive bana deta hai)
+// console.log(Math.round(4.6));//Math.round(x) → nearest integer round karta hai
 // console.log(Math.ceil(4.2)); //upper value
 // console.log(Math.floor(4.2)); // lower value
 // console.log(Math.min(4, 3, 6, 8)); 
