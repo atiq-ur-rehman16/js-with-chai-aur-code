7d03700896321db7bda1901b505ee0cba8279c52
Atiq Ur Rehman <114483766+atiq-ur-rehman16@users.noreply.github.com>
2025-08-21 14:39:43 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String , Number,  Boolean, null, undefiend, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let useEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 4356238592963278n
+
+
+
+// Reference (Non primitive)
+
+//Array , Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "goga"];
+let myObj ={
+    name:"atiq",
+    age: 22,
+}
+
+const myFunction = function (){
+console.log("Hello world");
+}
+
+console.log(typeof anotherId);
