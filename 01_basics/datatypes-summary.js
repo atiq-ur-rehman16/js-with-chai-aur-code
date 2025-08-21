2259e8f8a8e578dc7b95ea5f573eb31e2696f162
Atiq Ur Rehman <114483766+atiq-ur-rehman16@users.noreply.github.com>
2025-08-21 15:14:48 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -33,3 +33,32 @@ console.log("Hello world");
 }
 
 console.log(typeof anotherId);
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++
+
+// stack (primitive), heap (Non-primitive)
+
+let myYoutubename = " test"
+
+let anothername = myYoutubename
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@gamil.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "test@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
