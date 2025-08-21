60e848a33c8323759289a753dddc94269e2a19c0
Atiq Ur Rehman <114483766+atiq-ur-rehman16@users.noreply.github.com>
2025-08-21 12:26:05 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "atiqurrehman.devminds@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lahore"
+
+// accountId = 2
+
+accountEmail= "test@gmail.com"
+accountPassword ="212121212"
+accountCity = "islamabad"
+let accountState;
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3 + 3) we are using nodejs, not brower
+
+
+let name = "Atiq Ur Rehman"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => to power 53 
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
